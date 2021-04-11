976bb3b19b34404e2bbc7cbfd4567850163f9c71
Peter Vereshagin <peter@vereshagin.org>
2021-04-11 20:02:54 +0400

    feat(web): frontend

--- public/app.js
+++ public/app.js
@@ -1,26 +1,28 @@
-Vue.filter('formatDate', function(d) {
-	if(!window.Intl) return d;
-	return new Intl.DateTimeFormat('en-US').format(new Date(d));
-});
+Vue.filter("formatDate", function (d) {
+  if (!window.Intl) return d
+  return new Intl.DateTimeFormat("en-US").format(new Date(d))
+})
 
 const app = new Vue({
-	el:'#app',
-	data:{
-		term:'',
-		results:[],
-		noResults:false,
-		searching:false
-	},
-	methods:{
-		search:function() {
-			this.searching = true;
-			fetch(`https://itunes.apple.com/search?term=${encodeURIComponent(this.term)}&limit=10&media=music`)
-			.then(res => res.json())
-			.then(res => {
-				this.searching = false;
-				this.results = res.results;
-				this.noResults = this.results.length === 0;
-			});
-		}
-	}
-});
+  el: "#app",
+  data: {
+    searchString: "",
+    results: [],
+    noResults: false,
+    searching: false,
+  },
+  methods: {
+    search: function () {
+      this.searching = true
+      fetch(
+        `/api/v0/persons?searchString=${encodeURIComponent(this.searchString)}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          this.searching = false
+          this.data = res.data
+          this.noResults = this.data.length === 0
+        })
+    },
+  },
+})
